f40fa3ad94d0cd0721ee78d05e229faf408a152a
lucasgaleano <l.a.galeano2@gmail.com>
2018-06-28 00:44:27 -0300

    creo carpeta por tps

---
+++ tp1/archivos-tp1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(int argc, char* argv[]){
+
+
+   char buffer[100]; 
+
+   if(argc%2==0){
+      printf("cantidad de archivos incorrectos\n");
+      return 0;
+   }
+   
+   while(argc!=1){
+      argc = argc-2;
+
+      FILE* archivoLeer = fopen(*++argv,"r");
+      FILE* archivoEscribir = fopen(*++argv,"wb");
+
+      if(archivoLeer == NULL || archivoEscribir == NULL){
+         printf("error abriendo archivos\n");
+         return(-1);
+      }
+      printf("archivo %s copiado a %s \n",*(argv-1), *(argv));
+ 
+
+      while( (fgets(buffer,20,archivoLeer)) != NULL){
+          fputs(buffer,archivoEscribir);
+      }
+      
+      fclose(archivoLeer);
+      fclose(archivoEscribir);
+
+   }
+}
